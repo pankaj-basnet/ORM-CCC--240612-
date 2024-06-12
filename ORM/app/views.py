4473360b9fa20c76a0ac15ccf59e67a9c3b09432
Pankaj Basnet <pankajbasnet2020@hotmail.com>
2024-06-12 17:37:39 +0545

    next ----get single element of model---only one row with 'get'method

--- ORM/app/views.py
+++ ORM/app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+
+    # ...
+    # stu=Student.objects.all() #query set
+
+    # stu = Student.objects.filter(name="Shyam")
+    # stu = Student.objects.filter(name="shyam")
+    # stu = Student.objects.filter(age=60)
+
+    stu = Student.objects.filter(name="sujan")
+    stu=Student.objects.exclude(name="sujan")
+    stu=Student.objects.order_by('name')
+    stu=Student.objects.order_by('-age')
+    stu=Student.objects.order_by('age').reverse()
+    stu=Student.objects.order_by('?')
+    stu=Student.objects.values('name','age') [:2]
+    stu=Student.objects.values('name','age')
+
+    
+
+    return render(request, 'home.html', {'stu':stu})
