e129bae36f29aa49a6dfc8d63802434350c71e76
Pankaj Basnet <pankajbasnet2020@hotmail.com>
2024-06-13 15:59:16 +0545

    240612---CCC--hw-----Tech model added--

--- ORM/app/models.py
+++ ORM/app/models.py
@@ -6,3 +6,12 @@ class Student(models.Model):
     age=models.IntegerField()
     email=models.EmailField()
     address=models.CharField(max_length=200)
+
+## 340612---HW---CCC
+class Tech(models.Model):
+    name = models.CharField(max_length=200)
+    revenue1 = models.IntegerField()
+    employees = models.CharField(max_length=200)
+    revenue2 = models.IntegerField()
+    country = models.CharField(max_length=200)
+    headquarter = models.CharField(max_length=200)
